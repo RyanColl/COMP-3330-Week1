34f32e7b839817b5a0c933eaa238f337c6854f5c
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-09-11 01:46:48 -0700

    project complete

--- src/Display/Display.tsx
+++ src/Display/Display.tsx
@@ -7,7 +7,7 @@ const Display = (props: any) => {
         <div className={'display'} style={{width: width*0.8, height: height*0.8}}>
             <div className={'display-container'} style={lightThemed ? {backgroundColor: 'white', width: width*0.8, height: height*0.8} : {backgroundColor: '#2e2e2e', width: width*0.8, height: height*0.8} }>
                 <div className={'display-inner-container'} style={lightThemed ? {backgroundColor: '#2e2e2e', width: width*0.45, height: height*0.65} : {backgroundColor:'white', width: width*0.45, height: height*0.65}}>
-                    <div className='circle' style={lightThemed ? {backgroundColor: 'white'} : {backgroundColor: '#2e2e2e'} }></div>
+                    <div className={lightThemed ? 'circle' : 'circle-dark'} style={lightThemed ? {backgroundColor: 'white'} : {backgroundColor: '#2e2e2e'} }></div>
                     <h4 style={lightThemed ? {color: 'white'} : {color: '#2e2e2e'}} className='h4'>{lightThemed ? 'I am Light Themed!' : 'I am Dark Themed!'}</h4>
                     <button style={lightThemed ? {backgroundColor: 'white', color: '#2e2e2e'} : {backgroundColor: '#2e2e2e', color: 'white'}} onClick={buttonClick} className='button'>{`Click Me to Change to ${theme} Theme`}</button>
                 </div>
