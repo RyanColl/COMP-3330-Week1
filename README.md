d19bf68e40858dfdb02dc9871c1c858b124d3cf4
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-09-10 23:34:37 -0700

    working on readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### Week 1
 
-In week 1, we discussed the basics of React. I want to introduce to you a project that I am doing for COMP-3330 in week 1. I am going to build a simple app that uses typescript, a ternary oporator, state, and a button to change dark theme to light theme!
+In week 1, we discussed the basics of React. I want to introduce to you a project that I am doing for COMP-3330 in week 1. I am going to build a simple app that uses typescript, ternary oporators, state, and a button to change dark theme to light theme!
 
 #### Typescript
 
@@ -11,3 +11,11 @@ Typescript is involved in the project, but do not worry, simply using npm instal
 #### Ternary
 
 Ternary operator is used to check a condition that returns a boolean, and then executes code based on whether it is true or false.
+
+#### State
+
+State is used to update a component. Updating a component re-renders it, but with the re-rendered value. </br>
+For example, in the following code, we will destructure our state variable and our component-updating function (as described in the [docs](https://reactjs.org/docs/hooks-state.html)) and give it an initial value of a boolean.
+</br>
+
+<code>const [lightThemed, isLightThemed] = React.useState(true)</code>
--- src/Display/Display.tsx
+++ src/Display/Display.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import './display.css'
 const Display = (props: any) => {
     const { buttonClick, lightThemed, width, height } = props;
-    const theme = lightThemed ? 'light' : 'dark'
+    const theme = !lightThemed ? 'light' : 'dark'
     return(
         <div className={'display'} style={{width: width*0.8, height: height*0.8}}>
-            <div className={'display-container'} style={lightThemed ? {backgroundColor: 'white'} : {backgroundColor: '#2e2e2e'} }>
+            <div className={'display-container'} style={lightThemed ? {backgroundColor: 'white', width: width*0.8, height: height*0.8} : {backgroundColor: '#2e2e2e', width: width*0.8, height: height*0.8} }>
                 <div className={'display-inner-container'} style={lightThemed ? {backgroundColor: '#2e2e2e', width: width*0.45, height: height*0.65} : {backgroundColor:'white', width: width*0.45, height: height*0.65}}>
-                    <div className='circle' style={lightThemed ? {backgroundColor: 'white'} : {backgroundColor: '#2e2e2e'} }><span>☯</span></div>
+                    <div className='circle' style={lightThemed ? {backgroundColor: 'white'} : {backgroundColor: '#2e2e2e'} }></div>
                     <h4 style={lightThemed ? {color: 'white'} : {color: '#2e2e2e'}} className='h4'>{lightThemed ? 'I am Light Themed!' : 'I am Dark Themed!'}</h4>
                     <button style={lightThemed ? {backgroundColor: 'white', color: '#2e2e2e'} : {backgroundColor: '#2e2e2e', color: 'white'}} onClick={buttonClick} className='button'>{`Click Me to Change to ${theme} Theme`}</button>
                 </div>
